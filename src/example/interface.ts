f0b82fa9946386d50a3b8639f9a114bf78194a69
TianYio <1711839211@qq.com>
2019-12-02 14:23:42 +0800

    interface

--- src/example/interface.ts
+++ src/example/interface.ts
@@ -1,6 +1,7 @@
 interface INameInfo {
     firstName: string,
     lastName: string,
+
     [prop: string]: any
 }
 
@@ -12,6 +13,94 @@ const getFullName = ({firstName, lastName}: INameInfo) => {
 }
 getFullName({firstName: 'AHa', lastName: 'Du', size: 12});
 
-interface IAnimal {
+interface ClockConstructor {
+    new(hour: number, minute: number): ClockInterface;
+}
 
+interface ClockInterface {
+    tick();
+}
+
+function createClock(ctor: ClockConstructor, hour: number, minute: number): ClockInterface {
+    return new ctor(hour, minute);
+}
+
+class DigitalClock implements ClockInterface {
+    constructor(h: number, m: number) {
+    }
+
+    tick() {
+        console.log('beep beep')
+    }
+}
+
+class AnalogClock implements ClockInterface {
+    constructor(h: number, m: number) {
+    }
+
+    tick() {
+        console.log('tick tock')
+    }
+}
+
+let digital = createClock(DigitalClock, 12, 17);
+let analog = createClock(AnalogClock, 7, 32);
+
+interface Shape {
+    color: string;
+}
+
+interface Square extends Shape {
+    sideLength: number;
+}
+
+let square: Square = {
+    color: 'blue',
+    sideLength: 1
+};
+
+interface Counter {
+    (start: number): string;
+
+    interval: number;
+
+    reset(): void;
+}
+
+function getCounter(): Counter {
+    let counter = <Counter>function (start: number) {
+    };
+    counter.interval = 123;
+    counter.reset = function () {
+    };
+    return counter;
+}
+
+let c = getCounter();
+c(10);
+c.reset();
+c.interval = 5;
+
+class Control {
+    private state: any;
+    public content: string;
+}
+
+interface SelectableControl extends Control {
+    select(): void;
+}
+
+class Button extends Control implements SelectableControl {
+    select() {
+    }
+}
+
+class TextBox extends Control {
+
+}
+
+class Image implements SelectableControl {
+    //Property state、content from class Control is not implemented
+    select(): void {
+    }
 }
